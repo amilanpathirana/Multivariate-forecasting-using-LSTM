8d2138439e2a072a2b2a1509f70fbb0d9f4358d9
Amila <emailpathirana@gmail.com>
2021-01-21 10:51:32 -0600

    basic model with predictions

--- src/main.py
+++ src/main.py
@@ -2,10 +2,11 @@ import numpy as np
 import pandas as pd
 from keras.models import Sequential
 from keras.layers import LSTM
-from keras.layers import Dense
+from keras.layers import Dense, Dropout
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
 import seaborn as sns
+from keras.preprocessing.sequence import TimeseriesGenerator
 
 
 
@@ -50,37 +51,71 @@ print(df.tail())
 scaler=StandardScaler()
 df=scaler.fit_transform(df)
 
-print("First element of transformed dataframe \n")
+print("\nFirst element of transformed dataframe")
 print(df[0])
 print("\n")
 
-#As required for LSTM networks, we require to reshape an input data into n_samples x timesteps x n_features. 
-#In this example, the n_features is 2. We will make timesteps = 3. 
-#With this, the resultant n_samples is 5 (as the input data has 9 rows).
+# Train Validation and Test split
+n=len(df)
+
+train_df = df[0:int(n*0.7)]
+val_df = df[int(n*0.7):int(n*0.9)]
+test_df = df[int(n*0.9):]
+
+num_features = df.shape[1]
+
+
+
+
+#Input data must be in the shape n_samples x timesteps x n_features. 
 trainX = []
 trainY = []
 
-n_future = 2   # Number of samples we want to predict into the future
-n_past = 14     # Number of past samples we want to use for predicting
+n_future = 10   # Number of samples to the future
+n_past = 100    # Number of samples from the past
 
-
-
-for i in range(n_past, len(df) - n_future +1):
-    trainX.append(df[i - n_past:i, 0:df.shape[1]])
-    trainY.append(df[i + n_future - 1:i + n_future, 0])
+for i in range(n_past, len(train_df) - n_future +1):
+    trainX.append(train_df[i - n_past:i, 0:train_df.shape[1]])
+    trainY.append(train_df[i:i + n_future, 4])
 
 trainX, trainY = np.array(trainX), np.array(trainY)
 
 print('trainX shape == {}.'.format(trainX.shape))
 print('trainY shape == {}.'.format(trainY.shape))
+
+
+
+#model
+model = Sequential()
+model.add(LSTM(64, activation='relu', input_shape=(trainX.shape[1], trainX.shape[2]), return_sequences=True))
+model.add(LSTM(32, activation='relu', return_sequences=False))
+model.add(Dropout(0.2))
+model.add(Dense(trainY.shape[1]))
+
+model.compile(optimizer='adam', loss='mse')
+model.summary()
+
+
+
+# fit model
+history = model.fit(trainX, trainY, epochs=2, batch_size=16, validation_split=0.1, verbose=1)
+
+
     
+#Forecasting...
+#Start with the last day in training date and predict future...
+n_future=1500  #Redefining n_future to extend prediction dates beyond original n_future dates...
+
+forecast = model.predict(trainX[-n_future:]) #forecast 
+print(forecast)
+
+#Perform inverse transformation to rescale back to original range
+#Since we used 5 variables for transform, the inverse expects same dimensions
+#Therefore, let us copy our values 5 times and discard them after inverse transform
 
 
 
 
 
-
-
-
-
-
+plt.plot(forecast)    
+plt.show()             
